855696847f78c5c5f7b7b04d609707f39c436d54
Mason C <Mason@nonya.com>
2022-05-17 18:20:54 -0500

    Working on the model object

--- larascript/model.js
+++ larascript/model.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const args = process.argv.slice(2)
 
 
@@ -6,19 +7,36 @@ const checkForArgs = (arguments) => {
     arguments.forEach((val, index) => {
         if(val === '-m'){
             // Run migration.js
-
-            console.log('Creating Migration')
+            var fileContents = "console.log('i am a migration')\nconsole.log('testing some code')"
+            console.log('Creating Migration\n')            
+            fs.writeFile(`./database/create_${arguments[0]}_migration.js`, fileContents, function (err) {
+                if (err) throw err;
+                console.log('File is created successfully.');
+            })
         }
         if(val === '-c'){
             // Run controller.js
-
-            console.log('Creating Controller')
+            var fileContents = "console.log('i am a controller')\nconsole.log('testing some code')"
+            console.log('Creating Controller\n')                                    
+            fs.writeFile(`./app/Http/Controllers/${arguments[0]}Controller.js`, fileContents, function (err) {
+                if (err) throw err;
+                console.log('File is created successfully.');
+            })
         }
     })
 }
 
-const createNewModel = (args) => {
-    let model = args[0]
+checkForArgs(args)
 
+const createNewModel = (arguments) => {
+    var model_name = arguments[0]
+    var fileContents = "console.log('i am a model')\nconsole.log('testing some code')"
     
+    fs.writeFile(`./app/Models/${model_name}.js`, fileContents, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    })
 }
+
+
+createNewModel(args)
